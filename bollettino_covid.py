73f9bfbe780d8b9afb313de8d56997be9ec3ecbf
Pymai73 <51948373+Pymai73@users.noreply.github.com>
2022-01-01 13:53:06 +0100

    Update bollettino_covid.py

--- bollettino_covid.py
+++ bollettino_covid.py
@@ -22,19 +22,20 @@ dictionary_items = bollettino_covid.items()
 for item in dictionary_items:
     print(item)
 
-newurl=bollettino_covid['data'][:-9].replace('-','')  #elimino il trattino del formato data per avere il formato aaaammgg
+newurl=bollettino_covid['data'][:-9].replace('-','')  #sostituisco il trattino del formato data per avere 20211229
 datetime_object = datetime.strptime(newurl, '%Y%m%d') #trasformo la stringa newurl in data
-newurl = datetime_object - timedelta(days=1)          #sottraggo un giorno alla data definita in newurl
-newurl=str(newurl)[:10].replace('-','')               #tolgo il trattino divisore del formato aaaa-mm-gg
+newurl = datetime_object - timedelta(days=1) #sottraggo un giorno alla data definita in newurl
+newurl=str(newurl)[:10].replace('-','') #tolgo il trattino divisore del formato aaaa-mm-gg
 
 url2 = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-'
-url2=url2+newurl+'.csv'              #unisco la parte dell'url alla data del giorno precedente per avere il nuovo url
-data = pd.read_csv(url2)             #leggo il csv dal url del giorno precedente
+url2=url2+newurl+'.csv' #unisco la parte dell'url alla data del giorno precedente per avere il nuovo url
+data = pd.read_csv(url2)  #leggo il csv dal url del giorno precedente
 tamponi_prec=(int(data['tamponi']))  #prendo il numero di tamponi del giorno precedente
-positivita=bollettino_covid['nuovi_positivi']/(bollettino_covid['tamponi']-tamponi_prec) #calcolo il tasso di positività
+positivita=bollettino_covid['nuovi_positivi']/(bollettino_covid['tamponi']-tamponi_prec) #calcolo il tasso di positività nuovi/(differenza oggi e ieri dei tamponi)
 positivita_percentage = "{:.2%}".format(positivita)     #trasformo il decimale in percentuale prima di stampare il risultato
+deceduti_prec=(int(data['deceduti']))  #prendo il numero di decessi del giorno precedente
 
 print('\nI nuovi positivi in Italia per il giorno', bollettino_covid['data'][:-9], 'sono', bollettino_covid['nuovi_positivi'])
 print('Il numero di tamponi è pari a', bollettino_covid['tamponi']-tamponi_prec)
 print('Il tasso di positività è pari a', positivita_percentage)
-
+print('Il numero di decessi è', bollettino_covid['deceduti']-deceduti_prec)
