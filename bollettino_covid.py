d1eda0c03f093461fa2ab36acb357d432e0374f9
Pymai73 <51948373+Pymai73@users.noreply.github.com>
2022-01-08 18:41:42 +0100

    Update bollettino_covid.py

--- bollettino_covid.py
+++ bollettino_covid.py
@@ -38,5 +38,5 @@ deceduti_prec=(int(data['deceduti']))  #prendo il numero di decessi del giorno p
 print('\nI nuovi positivi in Italia per il giorno', bollettino_covid['data'][:-9], 'sono', bollettino_covid['nuovi_positivi'])
 print('Il numero di tamponi è pari a', bollettino_covid['tamponi']-tamponi_prec)
 print('Il tasso di positività è pari a', positivita_percentage)
-print('Il numero di ingressi/uscite dalla terapia intensiva è', bollettino_covid['ingressi_terapia_intensiva'])
+print('Il numero di ingressi dalla terapia intensiva è', bollettino_covid['ingressi_terapia_intensiva'])
 print('Il numero di decessi è', bollettino_covid['deceduti']-deceduti_prec)
