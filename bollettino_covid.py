49866d03fb7d23a8df76d0847cb804abb2d787e8
Pymai73 <51948373+Pymai73@users.noreply.github.com>
2022-01-12 18:44:10 +0100

    inserito if uscite terapia intensiva
    inserito if uscite terapia intensiva. In caso di diminuzione dei pazienti in terapia intensiva il print stamperà un messaggio diverso

--- bollettino_covid.py
+++ bollettino_covid.py
@@ -38,6 +38,9 @@ deceduti_prec=(int(data['deceduti']))  #prendo il numero di decessi del giorno p
 print('\nI nuovi positivi in Italia per il giorno', bollettino_covid['data'][:-9], 'sono', bollettino_covid['nuovi_positivi'])
 print('Il numero di tamponi è pari a', bollettino_covid['tamponi']-tamponi_prec)
 print('Il tasso di positività è pari a', positivita_percentage)
-print('Il numero di ingressi dalla terapia intensiva è', bollettino_covid['terapia_intensiva']-data['terapia_intensiva'].values[0])
+if bollettino_covid['terapia_intensiva']-data['terapia_intensiva'].values[0] >= 0:
+   print('Il numero di ingressi in terapia intensiva è', bollettino_covid['terapia_intensiva']-data['terapia_intensiva'].values[0])
+else:
+   print('Il numero di uscite dalla terapia intensiva è', bollettino_covid['terapia_intensiva']-data['terapia_intensiva'].values[0])
 print('Il numero di decessi è', bollettino_covid['deceduti']-deceduti_prec)
 print('\nseguite il progetto su https://github.com/Pymai73/bollettino_covid_giornaliero')
